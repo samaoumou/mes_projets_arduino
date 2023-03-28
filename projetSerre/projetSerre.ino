f98495cfbf2b7b3bd40e74e378944f2170893232
samaoumou <i.ndiaye@isepdiamniadio.edu.sn>
2023-03-28 15:10:17 +0000

    linkback

--- projetSerre/projetSerre.ino
+++ projetSerre/projetSerre.ino
@@ -1,56 +1,55 @@
-/*Groupe 4*/
-
+                                                                  /*Groupe 4 code de l'esp32*/
+                                               /*le keypad et l'écran lcd par préference seront géré par l'arduino Mega*/       
+/*ici on a l'ensemble des fichiers et bibliothèques utilisé*/
+              #include <ESP32Servo.h>
+              #include <LiquidCrystal.h>
+              #include <WiFi.h>
+              #include "secrets.h"
+              #include "DHT.h"
+              
+/*la déclaration des différents pins utilisé*/
+#define RED 14 //led tricolor(rouge)
+#define GRN 12    //led tricolor(vert)
+#define BLU 13  //led tricolor(blue)
+#define DHTTYPE DHT11   // DHT 11
+#define servo 19     //le pin du servoMoteur
+#define photo 36  // composante photoresistor sur le pin 36
+#define DHTPIN 23     // Digital pin connected to the DHT sensor
 int digitalPin = 33; // pour le relais
 int analogPin = 32; // pour le capteur de l'humidité du sol
 int digitalVal; // digital readings
 int analogVal; //analog readings
 
-#include <ESP32Servo.h>
-#include <LiquidCrystal.h>
-#include <WiFi.h>
-#define servo 11     //le pin du servoMoteur
-#define photo 36  // composante photoresistor sur le pin 36
-#include "secrets.h"
-#include "DHT.h"
-#define RED 14
-#define GRN 12
-#define BLU 13
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-#define DHTPIN 23     // Digital pin connected to the DHT sensor
+
 char ssid[] = SECRET_SSID;   // your network SSID (name) 
 char pass[] = SECRET_PASS;   // your network password
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 int ventilo = 22;
 int buzz = 15;
-unsigned int lumiere;
+
 Servo servoToit;     // on crée l'objet servoToit
 WiFiClient  client;
 DHT dht(DHTPIN, DHTTYPE);
 void setup() {
 
-  pinMode(digitalPin, OUTPUT); //pour initialiser le capteure de l'himudité
-  digitalWrite(digitalPin, LOW);
-  
-  servoToit.setPeriodHertz(50); // Fréquence PWM pour le SG90
-  servoToit.attach(11); // attache le servo au pin spécifié
-  pinMode(photo, INPUT);
-  pinMode(RED, OUTPUT);  // qui permet de manipuler la led tricolore avec les différentes couleures 
-  pinMode(GRN, OUTPUT); // et de les définir comme les sortie 
-  pinMode(BLU, OUTPUT);
-  Serial.begin(115200);  //Initialize serial 
-  Serial.println(F("DHTxx test!"));
-  pinMode(ventilo, OUTPUT);
-  pinMode(buzz, OUTPUT);
-  int analogValue = analogRead(photo);
-  
-  dht.begin();
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo native USB port only
-  }
-  
-  WiFi.mode(WIFI_STA);   
+      pinMode(digitalPin, OUTPUT); //pour initialiser le capteure de l'himudité
+      digitalWrite(digitalPin, LOW);
+      servoToit.setPeriodHertz(50); // Fréquence PWM pour le SG90
+      servoToit.attach(11); // attache le servo au pin spécifié
+      pinMode(photo, INPUT);
+      pinMode(RED, OUTPUT);  // qui permet de manipuler la led tricolore avec les différentes couleures 
+      pinMode(GRN, OUTPUT); // et de les définir comme les sortie 
+      pinMode(BLU, OUTPUT);
+      Serial.begin(115200);  //Initialize serial 
+      Serial.println(F("DHTxx test!"));
+      pinMode(ventilo, OUTPUT);
+      pinMode(buzz, OUTPUT);
+      int analogValue = analogRead(photo);
+      dht.begin();
+      while (!Serial) {
+        ; // wait for serial port to connect. Needed for Leonardo native USB port only
+      }
+      WiFi.mode(WIFI_STA);   
 }
 void loop() {
   delay(2000);
@@ -72,15 +71,15 @@ void loop() {
 
   }
     float value = analogRead(photo);
-  float h = dht.readHumidity();
+    float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
+    float t = dht.readTemperature();
   // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+    float f = dht.readTemperature(true);
 
-  // Check if any reads failed and exit early (to try again).
+  // lit et retourne moi une chaine de caractere en cas de problème de branchement.
   if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("erreur de donnée")); // le message affiché quand il y a une errreure de branchement
+    Serial.println(F("erreur de donnée")); // le message affiché quand il y a un mal branchement
        digitalWrite(RED, HIGH);
        digitalWrite(GRN, LOW);
        digitalWrite(BLU, LOW);
@@ -89,14 +88,16 @@ void loop() {
     return;
   }
   
-  float hif = dht.computeHeatIndex(f, h);
-  float hic = dht.computeHeatIndex(t, h, false);
-  Serial.print(F("Humidity: "));
+  //float hif = dht.computeHeatIndex(f, h);
+  //float hic = dht.computeHeatIndex(t, h, false);
+  Serial.print(F("HumiditéSerre: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
-  Serial.print(F(" luminosité: "));  
-  Serial.println(value);
+  Serial.print(F("°C luminosité: "));  
+  Serial.print(value);
+  Serial.print(F(" HumiditéSol: "));
+  Serial.println(analogVal);
 
    delay(5000);
 /*Cette partie consiste à faire l'arrosage automatique*/
